15d29e79562ef74dac441081d12f9eb86d970fd1
choticIdea <lana.shurtugal@gmail.com>
2018-01-27 11:13:26 +0700

    Spawner :D

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject[] spawnPlace;
+	public GameObject[] spawned;
+	public GameObject prefab;
+	public int safeBoxNum;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			Spawn ();
+		}
+	}
+	public void Spawn(){
+		if (safeBoxNum <= 0)
+			safeBoxNum = 0;
+		else if (safeBoxNum > spawnPlace.Length)
+			safeBoxNum = spawnPlace.Length;
+		spawned = new GameObject[safeBoxNum];
+		for (int i=0;i<safeBoxNum;i++){
+			spawned[i] = Instantiate(prefab,spawnPlace[i].transform.position,spawnPlace[i].transform.rotation);
+		}
+	}
+}
