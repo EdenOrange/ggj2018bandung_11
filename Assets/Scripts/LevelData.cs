26f601d82bf35a975b5aa78b83c09ab6cb5878a7
leecopper15 <ntnicholasthie@gmail.com>
2018-01-27 08:58:25 +0700

    Initial commit

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour {
+	/* Isi variabel-variabel yang berhubungan dengan game */
+
+	public int level;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
